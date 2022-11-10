8c774a178b1e38383ef372faf613086b8db32dff
kolawole olaniyi <kolaniyi3@gmail.com>
2022-11-10 14:58:51 +0100

    fixed deployment issue

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://yoursite.com";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://hulu-2-0-p2h85im4c-kolniy.vercel.app";
