9023c2aeded1e05005ded91bc3e1dc5ae7b19091
kolawole olaniyi <kolaniyi3@gmail.com>
2022-11-10 14:59:57 +0100

    fixed deployment

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://hulu-2-0-p2h85im4c-kolniy.vercel.app";
+  : "https://hulu-2-0-ten-pied.vercel.app";
